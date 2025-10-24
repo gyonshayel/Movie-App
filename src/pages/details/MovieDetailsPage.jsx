d27ea4610ab48703ca9ae59a5a848b8dab6b2a1a
Gyon Shayel Joshua <gyon.perera2002@gmail.com>
2025-10-24 23:06:29 +0400

    Add similar movies to the movie details page

--- src/pages/details/MovieDetailsPage.jsx
+++ src/pages/details/MovieDetailsPage.jsx
@@ -1,15 +1,30 @@
+import { useEffect } from "react";
+import { useParams } from "react-router";
 import { TopLevelDetails } from "./TopLevelDetails";
 import { VideoAndImages } from "./Video&Images";
 import { Cast } from "./Cast";
 import { Reviews } from "./Reviews";
+import { MovieList } from "../home/MovieList";
 
 export function MovieDetailsPage({ apiKey }) {
+  const { id } = useParams();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [id]);
+
   return (
     <>
       <TopLevelDetails apiKey={apiKey} />
       <VideoAndImages apiKey={apiKey} />
       <Cast apiKey={apiKey} />
       <Reviews apiKey={apiKey} />
+      <MovieList
+        key={id}
+        id={"similar-movies"}
+        url={`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${apiKey}`}
+        listName={"Similar Movies"}
+      />
     </>
   );
 }
