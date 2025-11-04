43d3f37a253d8e17e883ad16445a388a7d534673
Gyon Shayel Joshua <gyon.perera2002@gmail.com>
2025-11-04 13:38:07 +0400

    Style adjustments for the MovieCardSkeletonList

--- src/components/MovieCardSkeletonList.jsx
+++ src/components/MovieCardSkeletonList.jsx
@@ -4,7 +4,7 @@ import { Skeleton } from "./ui/skeleton";
 export function MovieCardSkeletonList({ count = 10 }) {
   return (
     <div>
-      <Skeleton className="h-6 w-[150px]" />
+      <Skeleton className="h-6 w-[120px] ml-2 mb-4" />
       <div className="flex overflow-x-auto gap-2 lg:gap-3 px-2 py-2 scrollbar-hide">
         {Array.from({ length: count }).map((_, index) => (
           <MovieCardSkeleton key={index} />
